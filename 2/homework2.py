e734755ec3b07ff393847620317ebed732f9843d
emrus <emrus0508@gmail.com>
2025-10-02 17:03:02 +0500

    task 2 finished

---
+++ 2/homework2.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from random import random, randint
+# task 1
+# name = input('Enter your name\n')
+# surname = input('Enter your name\n')
+# country = input('Where are you from?\n')
+# yob= int(input('Year of birth\n'))
+# def age_count():
+#     age = 2018 - yob
+#     return age
+#
+# age = age_count()
+#
+# def age_count_now():
+#     now_time_date = datetime.now()
+#     now_year_only = now_time_date.year
+#     age_now = int(now_year_only) - yob
+#     return age_now
+#
+# age_now = age_count_now()
+#
+#
+# print(f'Hello,  {name.capitalize()}  {surname.capitalize()} . You are  {age} years old. Your are living in {country.capitalize()}.')
+#
+# print(f'Hello,  {name.capitalize()}  {surname.capitalize()} . You are  {age_now} years old. Your are living in {country.capitalize()}.')
+
+# task 2
+# a = randint(1, 9)
+# b = randint(1, 9)
+#
+# equation = a * b
+#
+# answer = int(input(f'How much is {a} * {b} \n'))
+# if answer == equation:
+#     print('Correct')
+# else:
+#     print(f'Wrong. Correct answer is: {equation}')
+
+
+# task 3
+# print('Enter to strings : \n')
+#
+# str_1 = input()
+# str_2 = input()
+#
+# def dif(a,b):
+#     return len(a) - len(b)
+#
+# if dif(str_1,str_2) > 0:
+#     print(f'First string is longer by {dif(str_1,str_2)} characters ')
+# elif dif(str_1,str_2) < 0:
+#     print(f'Second string is longer by {-dif(str_1, str_2)} characters ')
+# else:
+#     print('Strings are equal')
+
+
+
+
+#     if len(str_1) > len(str_2):
+#         print(f'First string is longer by {len(str_1) - len(str_2)} characters ')
+#
+# if len(str_1) > len(str_2):
+#     print(f'First string is longer by {len(str_1) - len(str_2)} characters ')
+# elif len(str_1) < len(str_2):
+#     print(f'Second string is longer by {len(str_2) - len(str_1)} characters ')
+# else:
+#     print('Strings are equal length')
+
+# task 4
+#
+# 1. True
+# 2. True
+# 3. True
+# 4. False
